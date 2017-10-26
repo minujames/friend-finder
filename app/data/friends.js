dc4a99355e255b3174c60adacae67ebf56e5b657
Minu James <minujames@Minus-MacBook-Pro.local>
2017-10-26 09:51:58 -0700

    adding html and api route implementation

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var frindsList = [
+{
+  "name": "Jay",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+  "5",
+  "1",
+  "4",
+  "4",
+  "5",
+  "1",
+  "2",
+  "5",
+  "4",
+  "1"
+  ]
+}
+];
+
+function Friend(name, photo, scores){
+  this.name = name;
+  this.photo = photo;
+  this.scores = scores;
+}
+
+module.exports = frindsList;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,54 @@
+
+var friendsData = require("../data/friends.js");
+
+module.exports = function(app){
+
+  app.get("/api/friends", function(req, res){
+    res.json(friendsData);
+  });
+
+  app.post("/api/friends", function(req, res){
+    var newFriend = req.body;
+    console.log("friend", friend);
+
+    var compatibleFriends = findFriends(newFriend);
+
+    friendsData.push(friend);
+    res.json(compatibleFriends);
+  });
+}
+
+function findFriends(newFriend){
+  var compatibleFriends = [];
+  var prevFriendScore = Number.MAX_SAFE_INTEGER;
+
+  friendsData.forEach(function(friend){
+    var currFriendScore = getCompatibilityScore(friend, newFriend);
+    console.log(friend.name, currFriendScore);
+
+    if(currFriendScore === prevFriendScore){
+      compatibleFriends.push(friend);
+    }
+    else if(currFriendScore < prevFriendScore){
+      compatibleFriends = [];
+      compatibleFriends.push(friend);
+    }
+
+    prevFriendScore = currFriendScore;
+  });
+
+  return compatibleFriends;
+}
+
+function getCompatibilityScore(friend1, friend2){
+  return friend1.scores.reduce(function(total, score, currentIndex){
+    var friend_1_score = parseInt(score);
+    var friend_2_score = parseInt(friend2.scores[currentIndex]);
+    var diff = Math.abs(friend_1_score - friend_2_score);
+
+    return total + diff;
+  }, 0);
+}
+
+
+
